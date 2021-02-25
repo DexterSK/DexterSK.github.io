987109bb6bcf95448ec757c0a8668b69e79b789e
DexterSK <dextersk@azet.sk>
2021-02-25 12:24:33 +0100

    Update ShowMSnew.js

--- TribalWarsScripts/ShowMSnew.js
+++ TribalWarsScripts/ShowMSnew.js
@@ -3,18 +3,19 @@
 	// Príchod:	27.02. o 04:12:00
 	// Príchod:	zajtra 01:59:55
 	// Príchod:	dnes 13:52:03
-	
+	var found = false; 
 	var serverTime = document.getElementById("serverTime");
 	var relative_time = document.getElementsByClassName("relative_time");
 	var time = serverTime.innerHTML.match(/^\d+\:\d+\:\d+/);
 	
 	if( relative_time.length > 0 ) // elements with class "relative_time" exist
 		var time1 = relative_time[0].innerHTML.match(/([\s\S].*?)(\d+\:\d+\:\d+)/g);
-	/*else
+	else
 	{
-		alert("Class relative_time not found.");
-		return;
-	}*/
+		//alert("Class relative_time not found.");
+		//return;
+		found = true;
+	}
 	
 	var date = new Date();
 	var ms = ( date.getMilliseconds() ).toString();
@@ -23,7 +24,8 @@
 		ms="0"+ms;
 	
 	serverTime.innerHTML=time+":"+ms;
-	relative_time[0].innerHTML=time1+":"+ms;
+	if( found )
+		relative_time[0].innerHTML=time1+":"+ms;
 
 }(function main()
 {
