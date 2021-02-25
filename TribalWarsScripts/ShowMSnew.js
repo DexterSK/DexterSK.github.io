1dd200d5ad49c95b228e9699266af2d68397a048
DexterSK <dextersk@azet.sk>
2021-02-25 07:52:40 +0100

    Update ShowMSnew.js

--- TribalWarsScripts/ShowMSnew.js
+++ TribalWarsScripts/ShowMSnew.js
@@ -1,18 +1,1 @@
-﻿function setMS(){
-	var element=document.getElementById("serverTime");
-	//var dextersk=document.getElementsByClassName("relative_time");
-	var time=element.innerHTML.match(/^\d+\:\d+\:\d+/);
-	//var time1=dextersk.innerHTML.match(/^\d+\:\d+\:\d+/);
-	var date=new Date();
-	var ms=(date.getMilliseconds()).toString();
-	
-	while(ms.length<3)
-	{
-		ms="0"+ms;
-	};
-	
-	element.innerHTML=time+":"+ms;
-	//dextersk.innerHTML=time1+":"+ms;
-}(function main(){
-	window.setInterval(setMS,1);
-})();
+﻿function setMS(){var element=document.getElementById("serverTime");var time=element.innerHTML.match(/^\d+\:\d+\:\d+/);var date=new Date();var ms=(date.getMilliseconds()).toString();while(ms.length<3){ms="0"+ms;};element.innerHTML=time+":"+ms;}(function main(){window.setInterval(setMS,1);})();
