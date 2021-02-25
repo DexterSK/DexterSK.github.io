83c58e028390e4328d6a983020728a608527c2c6
DexterSK <dextersk@azet.sk>
2021-02-25 08:03:38 +0100

    Update ShowMSnew.js

--- TribalWarsScripts/ShowMSnew.js
+++ TribalWarsScripts/ShowMSnew.js
@@ -6,6 +6,8 @@
 	
 	if( relative_time.length > 0 ) // elements with class "relative_time" exist
 		var time1 = relative_time.innerHTML.match(/^\d+\:\d+\:\d+/);
+	else
+		alert("Class relative_time not found.");
 	
 	var date = new Date();
 	var ms = ( date.getMilliseconds() ).toString();
@@ -19,6 +21,8 @@
 	
 	if( relative_time.length > 0 ) // elements with class "relative_time" exist
 		relative_time.innerHTML=time+":"+ms;
+	else
+		alert("Class relative_time not found.");
 
 }(function main()
 {
