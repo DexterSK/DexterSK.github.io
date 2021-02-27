bb9abf98e48111c2b52122ea90e172a702dbf0d1
DexterSK <dextersk@azet.sk>
2021-02-27 19:11:01 +0100

    Update pasteAllMembers.js

--- TribalWarsScripts/pasteAllMembers.js
+++ TribalWarsScripts/pasteAllMembers.js
@@ -41,7 +41,7 @@
 	if( redirectToIncomingsPoint() )
 		return;
 	
-	document.getElementById('all').onclick();
+	document.getElementById('select_all').onclick();
 	document.getElementByName("label")[0].submit();
 	document.getElementByName("reqdef")[0].submit();
 		
