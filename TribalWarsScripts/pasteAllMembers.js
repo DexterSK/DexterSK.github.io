b02bf0139be0ca17ef170b2aa5989dfa080a5bf5
DexterSK <dextersk@azet.sk>
2021-02-27 09:09:36 +0100

    Create pasteAllMembers.js

---
+++ TribalWarsScripts/pasteAllMembers.js
@@ -0,0 +1,22 @@
+(() => 
+{
+	function redirectToMailPoint() {
+		function contains(str, substr) {
+			return str.indexOf(substr) >= 0;
+		}
+		let href = window.location.href;
+		let isMailPoint = contains(href, 'screen=mail') && (contains(href, 'mode=new') || !contains(href, 'mode='));
+		if (isMailPoint) {
+			return false;
+		} else {
+			if (confirm('Redirecting to rally point.')) {
+				let targetUrl = `/game.php?village=${game_data.village.id}&screen=mail&mode=new`
+				window.location.href = targetUrl;
+			}
+			return true;
+		}
+	}
+
+	var members = "147mirec;bonkourno;Eman;farmár0001;Jasur456;kubik14;Levov24;makaron371;maro1551;Nagato-Pein;Patko96;Riso2201;SexyKeksik25;ShelbyGT;Starý známy;Thanatos reborn;vrhacnozov;xXxPerúnxXx;samo15;dede53;Kramer;Kráľ granko;Dusky55;-Psycho-;Janik;roberto123;DarkSoulsCz;Vandroy05;smile;filip597;simeniik;roloo;Michal0132;remotust099;";
+	document.getElementById("to").value = members;
+})();
