00d6ddb3d05c4787bc6ca7115b700fe3778d519c
DexterSK <dextersk@azet.sk>
2021-02-27 13:27:15 +0100

    Update pasteAllMembers.js

--- TribalWarsScripts/pasteAllMembers.js
+++ TribalWarsScripts/pasteAllMembers.js
@@ -1,11 +1,5 @@
 (() => 
 {
-	//https://sk66.divoke-kmene.sk/game.php?village=6034&screen=reqdef&village_id=6034&id=92452811
-	function getAttacksData()
-	{
-		
-	}
-	
 	function redirectToMailPoint() {
 		function contains(str, substr) {
 			return str.indexOf(substr) >= 0;
@@ -15,7 +9,7 @@
 		if (isMailPoint) {
 			return false;
 		} else {
-			if (confirm('Presmerujem ťa na správy ty lenivé prasa.\nSpusti potom script znovu :)')) {
+			if (confirm('Presmerujem ťa na správy ty lenivé prasa.')) {
 				let targetUrl = `/game.php?village=${game_data.village.id}&screen=mail&mode=new`
 				window.location.href = targetUrl;
 			}
@@ -28,6 +22,5 @@
 
 
 	var members = "147mirec;bonkourno;Eman;farmár0001;Jasur456;kubik14;Levov24;makaron371;maro1551;Nagato-Pein;Patko96;Riso2201;SexyKeksik25;ShelbyGT;Starý známy;Thanatos reborn;vrhacnozov;xXxPerúnxXx;samo15;dede53;Kramer;Kráľ granko;Dusky55;-Psycho-;Janik;roberto123;Vandroy05;smile;filip597;simeniik;roloo;Michal0132;remotust099;";
-	
 	document.getElementById("to").value = members;
 })();
