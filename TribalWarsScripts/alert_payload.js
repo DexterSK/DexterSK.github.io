6c3dde92a0868851d7c2d2a7e5ef788be91f94a7
DexterSK <dextersk@azet.sk>
2022-11-08 04:50:58 +0100

    Update alert_payload.js

--- TribalWarsScripts/alert_payload.js
+++ TribalWarsScripts/alert_payload.js
@@ -1,12 +1,29 @@
 /**
  * Initialization
  
- window.pb=[];
- window.pb.WOOD_MIN=200,
- window.pb.STONE_MIN=200,
- window.pb.IRON_MIN=200,
- $.getScript('https://dextersk.github.io/TribalWarsScripts/alert_payload.js');
+javascript:
+window.pb=[];
+window.pb.WOOD_MIN=400,
+window.pb.STONE_MIN=400,
+window.pb.IRON_MIN=400,
+
+$.getScript('https://dextersk.github.io/TribalWarsScripts/alert_payload.js');
+
  */
+
+function redirectToExchangePoint() {
+	let href = window.location.href;
+	let isMailPoint = contains(href, 'screen=market') && (contains(href, 'mode=exchange') );
+	if (isMailPoint) {
+		return false;
+	} else {
+		//if (confirm('Budeš presmerovaný/á na stránku hromadnej správy.')) {
+			let targetUrl = `/game.php?village=${game_data.village.id}&screen=mail&mode=new`
+			window.location.href = targetUrl;
+		//}
+		return true;
+	}
+}
 
 // Override these settings by defining `window.pb`
 var settings = $.extend({
@@ -20,6 +37,7 @@ var settings = $.extend({
 console.log('Using settings: ', settings);
 	
 alert("NoFe@r's payload loaded!");
+redirectToExchangePoint();
 
 var wood = $('#premium_exchange_rate_wood').text().replace("                     ","").split("\n")[1];
 var stone = $('#premium_exchange_rate_stone').text().replace("                     ","").split("\n")[1];
